3c19f7c530b206a5db3d9243eac7cd7ff31b0334
Pierre Rossel <>
2025-10-02 16:21:03 +0200

    initial commit

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Ball : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log("racine: " + Mathf.Sqrt(4));
+    }
+
+    // detect collision with the target
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.CompareTag("Target"))
+        {
+            Debug.Log("Ball hit the target!");
+
+            // Load next scene in 2 seconds (so that the sound can play)
+            Invoke("LoadNextScene", 2f);
+        }
+    }
+
+    private void LoadNextScene()
+    {
+        // Load next scene or the first scene if we are at the last scene
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = (currentSceneIndex + 1) % SceneManager.sceneCountInBuildSettings;
+        SceneManager.LoadScene(nextSceneIndex);
+
+        //SceneManager.LoadScene("Level02");
+
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,46 @@
+# Board Game
+
+## Description
+
+## Ideas for future improvements
+
+## V 1.0
+
+[ ] Add sound effects
+  [x] hit
+  [ ] win
+  [ ] game over
+  [ ] score
+  [ ] power-up
+[ ] Detect ball loss
+[ ] Add levels
+[ ] Add bumpers
+[ ] Add colors and textures
+[ ] Add more obstacles
+  [ ] Add walls
+  [ ] Add moving obstacles
+  [ ] Add rotating obstacles
+  [ ] Add sliding obstacles (doors)
+  [ ] Add one-way walls
+  [ ] Add teleporters
+[ ] Add foes
+  [ ] Chasing foes
+  [ ] Random moving foes
+  [ ] Traps
+  [ ] Ejectors
+  [ ] Bombs
+
+## V 2.0
+
+[ ] Add transition between levels
+[ ] Add animations
+[ ] Add environment
+[ ] Add UI
+[ ] Add score
+[ ] Add timer
+[ ] Add more balls
+[ ] Add power-ups
+[ ] Add multiplayer
+[ ] Add restart button
+[ ] Add main menu
+[ ] Add lives
