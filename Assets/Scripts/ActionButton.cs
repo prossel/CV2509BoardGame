c4498bf0cad3073841cffd638ac2b7d7668bb0fb
Pierre Rossel <>
2025-10-16 14:19:33 +0200

    Add ActionButton script to trigger door animations on collision

---
+++ Assets/Scripts/ActionButton.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ActionButton : MonoBehaviour
+{
+
+    public Animator target;
+    public string triggerName = "";
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (target != null && triggerName != "")
+        {
+            target.SetTrigger(triggerName);
+        }
+    }
+}
