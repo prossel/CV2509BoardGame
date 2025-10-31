0b64dcff657b67c5b94b4ad7699e62f242994ea6
Pierre Rossel <>
2025-10-31 11:40:57 +0100

    Lock and hide cursor when a board is present

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -6,7 +6,19 @@ public class Board : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        
+    }
 
+    void OnEnable()
+    {
+        // Lock the cursor to the center of the screen
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void OnDisable()
+    {
+        // Unlock the cursor
+        Cursor.lockState = CursorLockMode.None;
     }
 
     // Update is called once per frame
@@ -17,14 +29,14 @@ public class Board : MonoBehaviour
             // Vector2 pos = Pointer.current.position.ReadValue();
             // Debug.Log("Pointer position: " + pos);
 
-           
+
             // rotate the board around the x axis when the mouse is moved up and down
             // and around the z axis when the mouse is moved left and right
             float rotationSpeed = 0.1f;
             float rotationX = Pointer.current.delta.y.ReadValue() * rotationSpeed;
             float rotationZ = -Pointer.current.delta.x.ReadValue() * rotationSpeed;
-            transform.Rotate(rotationX, 0, rotationZ, Space.World);            
-            
+            transform.Rotate(rotationX, 0, rotationZ, Space.World);
+
         }
     }
 
